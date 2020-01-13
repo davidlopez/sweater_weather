0b0639a0d792f9ced677297d5fa8abdab0e2e360
David Lopez <ldavid.x.lopezl@gmail.com>
2020-01-13 09:28:23 -0700

    Test setup

---
+++ spec/endpoints/antipode_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'I can recieve a current weather for a antipose city', :type => :request do
+  it 'Can return Location name and current forcash and search location', :vcr do
+    get '/api/v1/antipode?location=hongkong'
+
+    expect(response).to be_successful
+
+    city = JSON.parse(response.body, symbolize_names: true)[:attributes]
+
+    expect(city[:location]).to eq('La Quiaca, Jujuy, Argentina')
+  end
+end
