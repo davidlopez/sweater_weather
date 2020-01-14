ff7a9a1daafbacee9815038b3052591f1a279c0e
David Lopez <ldavid.x.lopezl@gmail.com>
2020-01-14 10:56:24 -0700

    Add test for no results

--- spec/endpoints/background_spec.rb
+++ spec/endpoints/background_spec.rb
@@ -13,4 +13,17 @@ describe "Retrieving a background image for a city", :type => :request do
     expect(image[:url]).to_not be_falsey
     expect(image[:url]).to be_a String
   end
+
+  it 'can use a default image unfound queries', :vcr do
+    get '/api/v1/backgrounds?location=asdfasdfa', headers: {
+      'Content-Type' => 'application/json',
+      'Accept' => 'application/json'
+    }
+
+    expect(response).to be_successful
+    image = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    expect(image[:url]).to_not be_falsey
+    expect(image[:url]).to eq('https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/weather/cloud/cumulus-cloud.jpg')
+  end
 end
