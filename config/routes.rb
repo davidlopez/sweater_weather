81751a17b591f707f23301a4f2cbea0a1c6114a1
David Lopez <ldavid.x.lopezl@gmail.com>
2020-01-14 09:32:35 -0700

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'weather#index'
       resources :users, only: :create
       resources :sessions, only: :create
+      resources :backgrounds, only: :show
     end
   end
 end
