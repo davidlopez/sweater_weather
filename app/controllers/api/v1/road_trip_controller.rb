fdfc270e2b2835d71b163dc44faa25d519396975
David Lopez <ldavid.x.lopezl@gmail.com>
2020-01-14 15:37:42 -0700

    Give status codes

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::RoadTripController < ApplicationController
+  def create
+    user = User.find_by(api_key: params[:api_key])
+    user ? roadtrip : failed
+  end
+
+  private
+
+    def roadtrip
+      render json: RoadtripSerializer.new(Roadtrip.new(params[:origin], params[:destination]))
+    end
+
+    def failed
+      render json: { failed: 'Unauthorized' }, status: 401
+    end
+end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    user && user.authenticate(params[:password]) ? login(user) : failed
+    user&.authenticate(params[:password]) ? login(user) : failed
   end
 
   private
 
     def login(user)
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(user), status: 200
     end
 
     def failed
